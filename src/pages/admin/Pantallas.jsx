523aaad2e34f40268e1ab274564b0021fa4b6c9d
AylineSanchez <ayline.07.01@gmail.com>
2024-06-10 21:45:35 -0400

    Cambios en login y pantallas
    Login: se agrega icono, colores y estilos a los elementos.
    Pantallas: se agregan estilos a los elementos.

--- src/pages/admin/Pantallas.jsx
+++ src/pages/admin/Pantallas.jsx
@@ -1,33 +1,33 @@
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-import { Button, Grid, Typography } from '@mui/material';
+import { Button, Grid, Typography, Paper } from '@mui/material';
 import { pantallas } from '../../assets/data/pantallas';
 import { CardCustomPantalla } from '../../components/admin/CardCustomPantalla';
 import { useNavigate } from 'react-router-dom';
 
 export const Pantallas = () => {
-
   const navigate = useNavigate();
 
-  const onEditPantalla = ( pantalla ) => {
-    navigate( '/admin/cambiar-mensaje-pantalla', { state: { pantalla } } );
+  const onEditPantalla = (pantalla) => {
+    navigate('/admin/cambiar-mensaje-pantalla', { state: { pantalla } });
   };
 
   return (
-    <>
-      <Grid container justifyContent="space-between">
+    <Grid container spacing={3} sx={{ p: 3 }}>
+      <Grid item xs={12} container justifyContent="space-between" alignItems="center">
         <Typography variant="h4">Pantallas</Typography>
-        <Button variant="contained" startIcon={ <AddCircleIcon /> }>
+        <Button variant="contained" startIcon={<AddCircleIcon />} sx={{ textTransform: 'none', fontWeight: 400 }}>
           Agregar
         </Button>
       </Grid>
-      <Grid container>
-        { pantallas.map( ( pantalla ) => (
-          <Grid item xs={ 12 } key={ pantalla.id }>
-            <CardCustomPantalla pantalla={ pantalla } accionIcono={ onEditPantalla } />
+      <Grid item xs={12} container spacing={2}>
+        {pantallas.map((pantalla) => (
+          <Grid item xs={12} key={pantalla.id}>
+            <Paper elevation={3} sx={{ p: 2, marginBottom: '16px' }}>
+              <CardCustomPantalla pantalla={pantalla} accionIcono={onEditPantalla} />
+            </Paper>
           </Grid>
-        ) ) }
-
+        ))}
       </Grid>
-    </>
+    </Grid>
   );
 };
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,129 +1,129 @@
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { Button, Grid, IconButton, InputAdornment, TextField, Typography } from '@mui/material';
+import { Button, Grid, IconButton, InputAdornment, TextField, Typography, Paper, Avatar } from '@mui/material';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { login } from '../../services/authService';
 import { CustomProgress } from '../../components/ui/CustomProgress';
 import { CustomAlert } from '../../components/ui/CustomAlert';
+import LockIcon from '@mui/icons-material/Lock';
 
 export const Login = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenAlert, setIsOpenAlert] = useState(false);
+  const [msgAlert, setMsgAlert] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-  const [ isOpen, setIsOpen ] = useState( false );
-  // const navigate = useNavigate();
-
-  // Alerta
-  //ALERT
-  const [ isOpenAlert, setIsOpenAlert ] = useState( false );
-  const [ msgAlert, setMsgAlert ] = useState( '' );
-
-  const [ showPassword, setShowPassword ] = useState( false );
-
-  const form = useForm( {
+  const form = useForm({
     defaultValues: {
       email: '',
       password: ''
     }
-  } );
+  });
 
   const { register, handleSubmit, formState } = form;
   const { errors } = formState;
 
   const handleCloseAlert = () => {
-    setIsOpenAlert( false );
+    setIsOpenAlert(false);
   };
 
   const onClickShowPassword = () => {
-    setShowPassword( !showPassword );
+    setShowPassword(!showPassword);
   };
 
-  const onSubmit = async ( data ) => {
-    setIsOpen( true );
+  const onSubmit = async (data) => {
+    setIsOpen(true);
 
-    const response = await login( data );
+    const response = await login(data);
 
-    if ( response.success ) {
+    if (response.success) {
       // navigate('/feed');
     } else {
-      setMsgAlert( 'Credenciales incorrectas' );
+      setMsgAlert('Credenciales incorrectas');
     }
-    setIsOpenAlert( true );
-    setIsOpen( false );
+    setIsOpenAlert(true);
+    setIsOpen(false);
   };
-
-  return (
-    <Grid
-      container
-      minHeight="100dvh"
-      width="100%"
-      bgcolor="primary.secondary"
-      alignItems="center"
-    >
+    return (
       <Grid
         container
-        gap={ 2 }
-        maxWidth="90%"
-        sx={ {
-          my: 5,
-          mx: 'auto'
-        } }
+        minHeight="100vh"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ backgroundColor: '#e0e0e0' }}
       >
-        <Grid item xs={ 8 } sx={ { mx: 'auto' } }>
-          <Typography variant="h4" fontWeight={ 500 } textAlign="center">
-            Anuncios LED UTALCA
-          </Typography>
-        </Grid>
-        <Grid item xs={ 12 }>
-          <TextField
-            autoComplete="off"
-            error={ !!errors.email }
-            helperText={ errors.email?.message }
-            name="email"
-            fullWidth
-            label="Correo electrónico"
-            { ...register( 'email', {
-              required: "El correo es requerido",
-            } ) }
-            type="email"
-          />
-        </Grid>
-        <Grid item xs={ 12 }>
-          <TextField
-            error={ !!errors.password }
-            helperText={ errors.password?.message }
-            name="password"
-            type={ showPassword ? 'text' : 'password' }
-            InputProps={ {
-              endAdornment: <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={ onClickShowPassword }
-                  edge="end"
-                >
-                  { showPassword ? <VisibilityOff /> : <Visibility /> }
-                </IconButton>
-              </InputAdornment>
-            } }
-            label="Contraseña"
-            { ...register( 'password', {
-              required: "La contraseña es requerida",
-            } ) }
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={ 12 }>
-          <Button
-            variant="contained"
-            fullWidth
-            size='large'
-            sx={ { py: 1, textTransform: 'none', fontWeight: 400, mt: 2 } }
-            onClick={ handleSubmit( onSubmit ) }
-          >
-            Ingresar
-          </Button>
-        </Grid>
+        <Paper elevation={10} sx={{ padding: 4, borderRadius: 2, width: '90%', maxWidth: 400, backgroundColor: "#d8e9fb" }}> 
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item>
+            <Avatar
+              src="https://ferretronica.com/cdn/shop/collections/Elementos_de_visualizacion_pantallas_lcd_display_7_segmentos_matrices_led_modulos_de_visualizacion_led_ferretronica_512x512.png?v=1537792596"
+              sx={{ width: 200, height: 200 }}
+            />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h5" fontWeight={500} textAlign="center">
+                Anuncios LED
+              </Typography>
+              <Typography variant="h5" fontWeight={500} textAlign="center">
+                UTALCA
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="off"
+                error={!!errors.email}
+                helperText={errors.email?.message}
+                name="email"
+                fullWidth
+                label="Correo electrónico"
+                {...register('email', {
+                  required: "El correo es requerido",
+                })}
+                type="email"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                error={!!errors.password}
+                helperText={errors.password?.message}
+                name="password"
+                type={showPassword ? 'text' : 'password'}
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={onClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }}
+                label="Contraseña"
+                {...register('password', {
+                  required: "La contraseña es requerida",
+                })}
+                fullWidth
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                fullWidth
+                size='large'
+                sx={{ py: 1, textTransform: 'none', fontWeight: 600, mt: 2, backgroundColor: '#1976d2', color: '#FFFFFF' }}
+                onClick={handleSubmit(onSubmit)}
+              >
+                Iniciar sesión
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+        <CustomAlert handleClose={handleCloseAlert} mensaje={msgAlert} tipoMensaje={'error'} open={isOpenAlert} />
+        <CustomProgress open={isOpen} />
       </Grid>
-      <CustomAlert handleClose={ handleCloseAlert } mensaje={ msgAlert } tipoMensaje={ 'error' } open={ isOpenAlert } />
-      <CustomProgress open={ isOpen } />
-    </Grid>
-  );
-};
+    );
+  };
+  
