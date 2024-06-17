f99fe36b7986d27bc41e5c6b020884dae5141c2c
AlonsoxNa <aorostica20@alumnos.utalca.cl>
2024-06-17 19:39:43 -0400

    refactor: se cambia el diseño de pantallas en vista usuarios

--- src/components/admin/CardCustomUsuario.jsx
+++ src/components/admin/CardCustomUsuario.jsx
@@ -1,57 +1,77 @@
-import AddLinkIcon from '@mui/icons-material/AddLink';
-import { Button, Card, CardActions, CardContent, IconButton, Typography } from '@mui/material';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { ModalAsociarPantalla } from './ModalAsociarPantalla';
+import AddLinkIcon from "@mui/icons-material/AddLink";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ModalAsociarPantalla } from "./ModalAsociarPantalla";
 
-
-export const CardCustomUsuario = ( { usuario } ) => {
+export const CardCustomUsuario = ({ usuario }) => {
   const navigate = useNavigate();
 
-  const [ openModal, setOpenModal ] = useState( false );
+  const [openModal, setOpenModal] = useState(false);
 
   const onClickUser = () => {
-    navigate( `/admin/pantallas-usuario`, { state: usuario } );
+    navigate(`/admin/pantallas-usuario`, { state: usuario });
   };
 
   const handleAsociarPantalla = () => {
-    setOpenModal( true );
+    setOpenModal(true);
   };
 
   const handleCloseModal = () => {
-    setOpenModal( false );
+    setOpenModal(false);
   };
 
   return (
-    <Card sx={ { minWidth: 275, mt: 4, boxShadow: 3 } } >
+    <Card sx={{ minWidth: 275, mt: 4, boxShadow: 3 }}>
       <CardContent>
-        <Typography variant="h5" component="h5" textAlign="center" sx={ { my: 1 } }>
-          { usuario.nombreCompleto }
+        <Typography
+          variant="h5"
+          component="h5"
+          textAlign="center"
+          sx={{ my: 1 }}
+        >
+          {usuario.nombreCompleto}
         </Typography>
       </CardContent>
-      <CardActions sx={ { justifyContent: 'center', gap: 4 } }>
+      <CardActions sx={{ justifyContent: "center", gap: 4 }}>
         <Button
           variant="contained"
           size="medium"
-          color={ usuario.habilitado ? "habilitado" : "deshabilitado" }
-          sx={ { textTransform: 'none', marginBottom: '24px' } }
+          color={usuario.habilitado ? "habilitado" : "deshabilitado"}
+          sx={{ textTransform: "none", marginBottom: "24px" }}
         >
-          { usuario.habilitado ? 'Habilitado' : 'Deshabilitado' }
+          {usuario.habilitado ? "Habilitado" : "Deshabilitado"}
         </Button>
         <Button
-          variant="contained"
+          variant="text"
           size="large"
-          color="tagSala"
-          sx={ { textTransform: 'none', marginBottom: '24px' } }
-          onClick={ onClickUser }
+          // color="tagSala"
+          sx={{ textTransform: "none", marginBottom: "24px" }}
+          onClick={onClickUser}
         >
-          { usuario.cantidadPantallas } pantallas
+          Ver pantallas
         </Button>
-        <IconButton sx={ { marginBottom: '24px' } } onClick={ handleAsociarPantalla } >
+        <IconButton
+          sx={{ marginBottom: "24px" }}
+          onClick={handleAsociarPantalla}
+        >
           <AddLinkIcon />
         </IconButton>
       </CardActions>
-      {openModal && <ModalAsociarPantalla open={ openModal } handleClose={ handleCloseModal } usuario={ usuario } />}
+      {openModal && (
+        <ModalAsociarPantalla
+          open={openModal}
+          handleClose={handleCloseModal}
+          usuario={usuario}
+        />
+      )}
     </Card>
   );
 };
