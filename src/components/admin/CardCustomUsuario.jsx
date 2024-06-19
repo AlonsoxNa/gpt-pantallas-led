ffbee396991625b9e66eb3711b788eb759b00815
AlonsoxNa <aorostica20@alumnos.utalca.cl>
2024-06-18 20:20:15 -0400

    refactor: se cambia de lugar el asociar pantalla para que tenga más sentido.

--- src/components/admin/CardCustomUsuario.jsx
+++ src/components/admin/CardCustomUsuario.jsx
@@ -1,31 +1,17 @@
-import AddLinkIcon from "@mui/icons-material/AddLink";
 import {
   Button,
   Card,
   CardActions,
   CardContent,
-  IconButton,
-  Typography,
+  Typography
 } from "@mui/material";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { ModalAsociarPantalla } from "./ModalAsociarPantalla";
 
 export const CardCustomUsuario = ({ usuario }) => {
   const navigate = useNavigate();
 
-  const [openModal, setOpenModal] = useState(false);
-
   const onClickUser = () => {
     navigate(`/admin/pantallas-usuario`, { state: usuario });
-  };
-
-  const handleAsociarPantalla = () => {
-    setOpenModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setOpenModal(false);
   };
 
   return (
@@ -58,20 +44,8 @@ export const CardCustomUsuario = ({ usuario }) => {
         >
           Ver pantallas
         </Button>
-        <IconButton
-          sx={{ marginBottom: "24px" }}
-          onClick={handleAsociarPantalla}
-        >
-          <AddLinkIcon />
-        </IconButton>
       </CardActions>
-      {openModal && (
-        <ModalAsociarPantalla
-          open={openModal}
-          handleClose={handleCloseModal}
-          usuario={usuario}
-        />
-      )}
+
     </Card>
   );
 };
--- src/pages/admin/PantallasDeUsuario.jsx
+++ src/pages/admin/PantallasDeUsuario.jsx
@@ -5,38 +5,57 @@ import { useLocation } from 'react-router-dom';
 import { CardCustomPantalla } from '../../components/admin/CardCustomPantalla';
 import { usePantallasUsuario } from '../../hooks/admin/usePantallasUsuario';
 import { CustomProgress } from '../../components/ui/CustomProgress';
+import { useState } from 'react';
+import { ModalAsociarPantalla } from '../../components/admin/ModalAsociarPantalla';
 
 export const PantallasDeUsuario = () => {
 
   const location = useLocation();
   const usuario = location.state;
 
-  const { isLoading, pantallasUsuario } = usePantallasUsuario( usuario.id );
+  const [openModal, setOpenModal] = useState(false);
 
-  const onDesasociarPantalla = ( pantalla ) => {
-    console.log( 'Desasociar pantalla con id:', pantalla.id );
+  const { isLoading, pantallasUsuario } = usePantallasUsuario(usuario.id);
+
+  const onDesasociarPantalla = (pantalla) => {
+    console.log('Desasociar pantalla con id:', pantalla.id);
+  };
+
+  const handleAsociarPantalla = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
   };
 
   return (
     <>
-      <CustomProgress open={ isLoading } />
+      <CustomProgress open={isLoading} />
       <Grid container justifyContent="space-between" alignItems="center">
-        <Grid item xs={ 8 }>
-          <Typography variant="h4">Pantallas asociadas a { usuario.nombreCompleto }</Typography>
+        <Grid item xs={8}>
+          <Typography variant="h4">Pantallas asociadas a {usuario.nombreCompleto}</Typography>
         </Grid>
-        <Grid item xs={ 4 }>
-          <Button variant="contained" startIcon={ <AddLinkIcon /> }>
+        <Grid item xs={4}>
+          <Button variant="contained" startIcon={<AddLinkIcon />} onClick={handleAsociarPantalla}>
             Asociar
           </Button>
         </Grid>
         <Grid container>
-          { pantallasUsuario.map( ( pantalla, index ) => (
-            <Grid item xs={ 12 } key={ index }>
-              <CardCustomPantalla pantalla={ pantalla.pantalla } icono={ <LinkOffIcon fontSize="large" /> } accionIcono={ onDesasociarPantalla } />
+          {pantallasUsuario.map((pantalla, index) => (
+            <Grid item xs={12} key={index}>
+              <CardCustomPantalla pantalla={pantalla.pantalla} icono={<LinkOffIcon fontSize="large" />} accionIcono={onDesasociarPantalla} />
             </Grid>
-          ) ) }
+          ))}
         </Grid>
       </Grid>
+      {openModal && (
+        <ModalAsociarPantalla
+          open={openModal}
+          handleClose={handleCloseModal}
+          usuario={usuario}
+        />
+      )}
     </>
   );
 };
